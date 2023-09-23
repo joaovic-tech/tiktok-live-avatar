22bc9e1a0fd2029c5165c83be681c63b01597b9e
João Victor <joaovictorca2004@gmail.com>
2023-09-23 14:05:15 +0000

    update Readme.md e server.ts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tiktok-live-npc
+# tiktok-live-avatar
 
 [![Em Desenvolvimento](https://img.shields.io/badge/Status-Em%20Desenvolvimento-yellow)](https://github.com/joaovic-tech/tiktok-live-npm)
 [![Licença](https://img.shields.io/badge/Licença-GNU%20GENERAL%20PUBLIC%20LICENSE%20Version%203-blue)](./LICENSE)
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,6 +1,6 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
-// import TiktokLive from "./api/TiktokLive";
+import TiktokLive from "./api/TiktokLive";
 import { getAllChat } from "./routes/get-all-chat";
 import { getAllGift } from "./routes/get-all-gift";
 
@@ -11,8 +11,8 @@ app.register(fastifyCors, {
   origin: "*", // Altere para a URL de produção no futuro.
 });
 
-// const tiktokLive = new TiktokLive("danioafg");
-// tiktokLive.connect();
+const tiktokLive = new TiktokLive("danioafg");
+tiktokLive.connect();
 
 // routes
 app.register(getAllChat);
